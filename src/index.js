85cb6446c87c08fb54808228cf71c89b64973bde
kunal <kunal@gmail.com>
2025-07-04 22:18:19 +0530

    utils set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 // require("dotenv").config({ path: "./env" });
 // ___ notes we can run this env code there is no problem in this code but i am using the import statement toh yee hammare code ki consistency kharab kar raha hai....
 import dotenv from "dotenv"; //___ notes if we want to use like this with import statement so we have to change our package.json file  in we have to write in dev     nodemon -r dotenv/config --experimental-json-modules src/index.js ....
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 import { connectDB } from "./db/index.js";
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Sever is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failed !!!", error);
+  });
 
 // import express from "express";
 // const app = express();
